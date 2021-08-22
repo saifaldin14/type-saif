306c63f9836dc0a92b682bd3a0ee5b9eb8799e4d
saifaldin14 <saifaldindeveloper@gmail.com>
2021-08-21 20:03:43 -0400

    Update Header

--- components/Header.js
+++ components/Header.js
@@ -4,19 +4,27 @@ import Icon from "@material-tailwind/react/Icon";
 
 const Header = () => {
   return (
-    <div className="flex items-center">
+    <div className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
       <Button
         color="gray"
         buttonType="outline"
         rounded={true}
         iconOnly={true}
         ripple="dark"
-        className="md:inline-flex h-20 w-20 border-0"
+        className="h-20 w-20 border-0"
       >
         <Icon name="menu" size="3xl" />
       </Button>
       <Icon name="description" size="5xl" color="blue" />
-      <h1 className="md:inline-flex ml-2 text-gray-700">Docs</h1>
+      <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
+      <div className="flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-500 rounded-lg">
+        <Icon name="search" size="3xl" color="gray" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-grow px-5 text-sm bg-transparent outline-none"
+        />
+      </div>
     </div>
   );
 };
